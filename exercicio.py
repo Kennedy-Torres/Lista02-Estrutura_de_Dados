85dc75fd9a21a65d813eeaadfa66e8b30a00cd4b
Kennedy-Torres <krtorres.programador@hotmail.com>
2023-10-19 17:30:49 -0300

    ADD -> METODOS e ORDEM da listagem de produtos
    METODOS - Menu Op.03 - listagem dos produto...
    ORDEM - Menu Op.03 - listagem dos produtos...

--- exercicio.py
+++ exercicio.py
@@ -80,15 +80,66 @@ def excluirProduto(produtos, idEscolhidoParaExcluirProduto):
     print(f"Nenhum produto encontrado com o ID {idEscolhidoParaExcluirProduto}.")
 #--
 
+#Metodos de ordenacao
+def bubble_sort(produtos):
+    qtd_produtos = len(produtos)
+    for i in range(qtd_produtos-1):
+        for j in range(0, qtd_produtos-i-1):
+            if produtos[j]['Nome'] > produtos[j+1]['Nome']:
+                produtos[j], produtos[j+1] = produtos[j+1], produtos[j]
+
+def insertion_sort(produtos):
+    for i in range (1, len(produtos)):
+        chave = produtos[i]
+        j = i-1
+        while j>=0 and chave['Nome'] < produtos[j]['Nome']:
+            produtos[j+1] = produtos[j]
+            j -= 1
+        produtos[j+1] = chave
+    
+def selection_sort(produtos):
+    qtd_produtos = len(produtos)
+    for i in range(qtd_produtos):
+        min_idx = i
+        for j in range(i + 1, qtd_produtos):
+            if produtos[j]['Nome'] < produtos[min_idx]['Nome']:
+                min_idx = j
+        produtos[i], produtos[min_idx] = produtos[min_idx], produtos[i]
+#--
+    
+# Função para listar produtos pelo metodo
+def listarProdutosEmMetodos(produtos):
+    metodos = {'a': bubble_sort,'b': insertion_sort,'c': selection_sort} # atraves desse dicionario da pra eu filtrar qual funcao eu vou chamar 
+    metodo_escolhido = input("METODOS:\n(a) - Bubble Sort\n(b) - Insertion Sort\n(c) - Selection Sort\nEscolha o método(a, b, c): ").lower()
+    #validacao do metodo q serah ordenado os produtos
+    while metodo_escolhido not in ['a', 'b', 'c']:
+        print("Opção inválida para o metodo. Tente novamente.")
+        metodo_escolhido = input("METODOS:\n(a) - Bubble Sort\n(b) - Insertion Sort\n(c) - Selection Sort\nEscolha o método(a, b, c): ").lower()
+    
+    
+    if metodo_escolhido in metodos: # filtrando
+        metodo_escolhido = metodos[metodo_escolhido] #sobrescreve os dados de metodo_escolhido... a/b/c = bubble_sorte/insertion_sort/selection_sort 
+        produtos_ordenados = produtos.copy()  # Copia a lista de produtos para evitar alterações na lista original
+        metodo_escolhido(produtos_ordenados)  # Ordena a cópia dos produtos ... bubble_sort/insertion_sort/selection_sort(produtos_ordenados)
+        listarProdutosOrdemAlfabetica(produtos_ordenados)
+        ##RETIRAR DEPOIS ... !!!!!!!!!!!!!!!
+        #for produto in produtos_ordenados:
+        #    print(produto)
+    
+    
 # Função para listar produtos em ordem alfabética (crescente ou decrescente)
-def listarProdutosOrdemAlfabetica(produtos, ordem='crescente'):
-    if ordem == 'crescente':
+def listarProdutosOrdemAlfabetica(produtos):
+    #validação da Ordem dos produtos 
+    ordem = input("ORDEM:\n(d) - crescente\n(e) - decrescente\nEscolha a ordem(d, e): ").lower()
+    while ordem not in ['d', 'e']:
+        print("Opção inválida para a ordem. Tente novamente.")
+        ordem = input("ORDEM:\n(d) - crescente\n(e) - decrescente\nEscolha a ordem(d, e): ").lower()
+
+    
+    if ordem == 'd':
         produtos_ordenados = sorted(produtos, key=lambda x: x['Nome'])
-    elif ordem == 'decrescente':
-        produtos_ordenados = sorted(produtos, key=lambda x: x['Nome'], reverse=True)
     else:
-        print("Opção inválida para a ordem de classificação.")
-        return
+        produtos_ordenados = sorted(produtos, key=lambda x: x['Nome'], reverse=True)
 
     if produtos_ordenados:
         for produto in produtos_ordenados:
@@ -138,8 +189,8 @@ def main():
             else:
                 print("Nenhum produto encontrado com os IDs especificados.")
         elif opMenu == '3':
-            ordem = input("Digite 'crescente' para ordem crescente ou 'decrescente' para ordem decrescente: ").lower()
-            listarProdutosOrdemAlfabetica(produtos, ordem)
+            
+            listarProdutosEmMetodos(produtos)
         elif opMenu == '4':
             nomeProduto = input("Digite o nome do produto: ")
             pesoProduto = float(input("Digite o peso do produto: "))
